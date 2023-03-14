4ed579ff36b246e1a1ba458977c3c83f69019b60
Ryan Lin <ryanlin@ryans-mbp-9.wifi.local.cmu.edu>
2023-03-14 15:45:45 -0400

    what is your name?

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,5 +7,9 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("what is your name?")) {
+    return ("Ryan2");
+  }
+
   return "";
 }
